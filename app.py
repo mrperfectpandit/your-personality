3ab2f8f4f8c71f07df8449c8d581609fd38fe0e9
mrperfectpandit <48547461+mrperfectpandit@users.noreply.github.com>
2021-05-30 08:07:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,5 @@ def follow_tweet():
 if __name__ == '__main__' :
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    app.run(debug=True , port = 5000)
+    app.run()
+#     app.run(debug=True , port = 5000)
